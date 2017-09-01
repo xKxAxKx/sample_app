60bac9affcab92f2d31f27029cb89b5d19bc518d
xKxAxKx <pekepooooong@gmail.com>
2017-09-01 10:18:52 +0000

    ・ログイン失敗時の処理を扱う (誤りあり)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
       # エラーメッセージを作成する
       render 'new'
     end
