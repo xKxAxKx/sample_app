afe586e5e66ea136329553d43cabecc620d8d4f5
xKxAxKx <pekepooooong@gmail.com>
2018-02-16 09:57:42 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,9 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
+  # ユーザのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
