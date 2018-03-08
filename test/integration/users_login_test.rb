e0594b2084dba3254f169dff80c5e7c7f2eeb8ad
xKxAxKx <pekepooooong@gmail.com>
2018-03-08 10:43:50 +0000

    update

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -38,4 +38,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
   end
   
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+    puts(cookies['remember_token'])
+  end
+
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    puts(cookies['remember_token'])
+    assert_empty cookies['remember_token']
+  end
+  
 end
