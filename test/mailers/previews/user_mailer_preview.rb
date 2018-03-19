e0adeced09ea65689d1f09167a6178218fc18a54
xKxAxKx <pekepooooong@gmail.com>
2018-03-19 10:50:19 +0000

    11.2.2 送信メールのプレビュー

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,19 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# require 'test_helper'
+
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # Preview this email at
+  # http://rails-tutorial-xkxaxkx.c9users.io/rails/mailers/user_mailer/account_activation
   def account_activation
-    UserMailer.account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    puts('hogehoge')
+    UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at
+  # http://rails-tutorial-xkxaxkx.c9users.io/rails/mailers/user_mailer/password_reset
   def password_reset
     UserMailer.password_reset
   end
-
 end
