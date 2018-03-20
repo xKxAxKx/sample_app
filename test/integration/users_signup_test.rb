be169746811a6eebf753984478be799604d208dc
xKxAxKx <pekepooooong@gmail.com>
2018-03-20 03:00:02 +0000

    11.2.4 ユーザーのcreateアクションを更新

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
+    # assert_template 'users/show'
   end
 end
