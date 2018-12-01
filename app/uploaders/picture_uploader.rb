d41d02b192be2881aea4833d732bd382e7d9c79a
xKxAxKx <pekepooooong@gmail.com>
2018-12-01 04:39:24 +0000

    modified:   app/uploaders/picture_uploader.rb

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,8 @@
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
